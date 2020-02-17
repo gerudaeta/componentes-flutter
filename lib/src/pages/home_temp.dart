8e211beb17c0999ec7a9fb9e850350e0d7d2ebd7
German <gerudaeta@gmail.com>
2020-02-17 00:30:16 -0300

    + ListView - mostrando una lista de elementos

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('ListTile title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile title'),
+          )
+        ],
+      ),
+    );
+  }
+
+}
